396519271451571be0d87e4e3f83e18165cfecce
Javier López de Ancos <javierlopezdeancos@outlook.com>
2021-04-11 20:06:28 +0200

    test: add get test to flights

---
+++ __tests__/flights/index.ts
@@ -0,0 +1,52 @@
+import {
+  createRequest,
+  createResponse,
+} from 'node-mocks-http'
+
+import API from '../../routes'
+import { FLIGHTS } from '../../routes/flights'
+import { Flight } from '../../types/flight'
+import FlightModel from '../../models/flight'
+import flightsMock from '../../mocks/flights'
+import { getAirportsHandler } from '../../controllers/airports'
+
+const FLIGHTS_API_ROUTE = `${API}${FLIGHTS}`
+
+describe(`${FLIGHTS_API_ROUTE}`, () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  })
+
+  test("should responds 200 when GET", async () => {
+    jest.spyOn(FlightModel, 'find').mockImplementationOnce((): Promise<Flight[]> => Promise.resolve(flightsMock))
+
+    const request  = createRequest({
+      method: 'GET',
+      url: FLIGHTS_API_ROUTE,
+  });
+
+    const response = createResponse();
+    await getAirportsHandler(request, response)
+
+    expect(response.statusCode).toBe(200)
+  })
+
+  test("responds with birds in BBDD when GET", async () => {
+    jest.spyOn(FlightModel, 'find').mockImplementationOnce((): Promise<Flight[]> => Promise.resolve(flightsMock))
+
+    const request  = createRequest({
+      method: 'GET',
+      url: FLIGHTS_API_ROUTE,
+  });
+
+    const response = createResponse();
+    await getAirportsHandler(request, response)
+    const flights = response._getJSONData()
+
+    expect(flights).toMatchObject(flightsMock)
+  })
+
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+})
