e902d7f9db6430c25161f3836de73a7a0b32e9b2
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2022-03-26 18:40:38 +0100

    chore: upgrade to Next12

--- middleware/mongodb.ts
+++ middleware/mongodb.ts
@@ -1,20 +1,16 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const connectDB = (handler: (req: NextApiRequest, res: NextApiResponse) => void) =>
+const connectDB =
+  (handler: (req: NextApiRequest, res: NextApiResponse) => void) =>
   async (req: NextApiRequest, res: NextApiResponse) => {
     if (mongoose.connections[0].readyState) {
       // Use current db connection
       return handler(req, res);
     }
     // Use new db connection
-    await mongoose.connect(process.env.MONGODB_URL, {
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-      useNewUrlParser: true
-    });
+    await mongoose.connect(process.env.MONGO_DB_URL);
 
     return handler(req, res);
   };
